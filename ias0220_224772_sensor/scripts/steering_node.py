00acf7e9204a0be2107a8083841d7b6a17c36bbe
Evgenia Vesterblom <evgenia.udod@ttu.ee>
2022-11-13 02:23:03 -0500

    add distance to subscriber

--- ias0220_224772_sensor/scripts/steering_node.py
+++ ias0220_224772_sensor/scripts/steering_node.py
@@ -3,33 +3,37 @@
 import rospy
 from sensor_msgs.msg import Imu
 from geometry_msgs.msg import Twist, Vector3
+from std_msgs.msg import Int16
 import tf_conversions
 import math
 
 class SteeringNode():
     def __init__(self):
-        rospy.Subscriber("/imu", Imu, self.imu_callback, queue_size = 200)
+        rospy.Subscriber("/imu", Imu, self.calculate_vel_callback, queue_size = 200)
+        rospy.Subscriber("/distance", Int16, self.set_distance_callback, queue_size = 200)
         self.pub_twist = rospy.Publisher('/diff_drive_controller/cmd_vel', Twist, queue_size=10)
         self.twist = Twist()
         self.rate = rospy.Rate(30) 
+        self.distance = Int16()
 
-    def imu_callback(self, data):
-        quaternion = [data.orientation.x,
-                        data.orientation.y,
-                        data.orientation.z,
-                        data.orientation.w]
-        roll, pitch, yaw = tf_conversions.transformations.euler_from_quaternion(quaternion)
+    def set_distance_callback(self, data):
+        self.distance = data
+
+    def calculate_vel_callback(self, data):
+        quaternion = [data.orientation.x,data.orientation.y,data.orientation.z,data.orientation.w]
+        roll, pitch, _ = tf_conversions.transformations.euler_from_quaternion(quaternion)
         pitch_deg = math.degrees(pitch)
         roll_deg = math.degrees(roll)
 
         velocity_lin = self.calculate_speed(pitch_deg)
         linear_velocity = Vector3(velocity_lin,0,0)
-
         velocity_ang = self.calculate_speed(roll_deg)
         angular_velocity = Vector3(0,0,-velocity_ang)
 
-        self.twist.linear = Vector3(0.6,0, 0)#linear_velocity
+        self.twist.linear = linear_velocity
         self.twist.angular = angular_velocity
+
+        rospy.loginfo('Info %s %s', pitch_deg, self.distance)
 
         self.pub_twist.publish(self.twist)
         self.rate.sleep()
